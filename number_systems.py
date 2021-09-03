a9260c46b924a83686614543a4fc1d2619317c05
Grachev Anton <antonio.grachev@gmail.com>
2021-09-03 19:33:02 +0300

    Added fractions

--- number_systems.py
+++ number_systems.py
@@ -13,7 +13,25 @@ def convert_base(num, from_base=10, to_base=10):
         if i.islower():
             num = num.replace(i, i.upper())
 
+    if num.count('.') > 1 or num.count(',') > 1:
+        return 'Введите корректную дробь'
+
+    frac = ''
+    if '.' in num:
+        num, frac = num.split('.')
+    elif ',' in num:
+        num, frac = num.split(',')
+
     for i in num:
+        if i.isalpha():
+            i = ord(i) - 55
+        try:
+            if int(i) > from_base:
+                return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+        except:
+            return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+
+    for i in frac:
         if i.isalpha():
             i = ord(i) - 55
         try:
@@ -31,9 +49,28 @@ def convert_base(num, from_base=10, to_base=10):
     if from_base == 10 and to_base != 10:
         res, num = '', int(num)
         while num > 0:
-            plus = chr(65 + num % to_base - 10) if num % to_base > 9 else num % to_base
+            plus = chr(55 + num % to_base) if num % to_base > 9 else num % to_base
             res = str(plus) + res
             num //= to_base
+        if frac != '':
+            res += '.'
+            frac = float('0.' + frac)
+            n = 0
+            while n < 10:
+                frac *= to_base
+                tmp, frac_tmp = str(frac).split('.')
+                frac = float('0.' + frac_tmp)
+                tmp = int(tmp)
+                res += chr(55 + tmp) if tmp % to_base > 9 else str(tmp)
+                n += 1
+                if frac_tmp == '0':
+                    return str(res)
+            res, s = res.split('.')
+            i = (s + s).find(s, 1, -1)
+            if i != -1:
+                res += '.(' + s[:i] + ')'
+            else:
+                res += '.' + s + ' (округлено до 10 символов)'
         return str(res)
 
     elif from_base != 10 and to_base == 10:
@@ -43,6 +80,13 @@ def convert_base(num, from_base=10, to_base=10):
                 res += (ord(num[len(num) - 1 - i]) - 55) * (from_base ** i)
             else:
                 res += int(num[len(num) - 1 - i]) * (from_base ** i)
+
+        if frac != '':
+            for i in range(0, len(frac)):
+                if frac[i].isalpha():
+                    res += (ord(frac[i]) - 55) * (from_base ** -(i + 1))
+                else:
+                    res += int(frac[i]) * (from_base ** -(i + 1))
         return str(res)
 
     elif from_base != 10 and to_base != 10:
