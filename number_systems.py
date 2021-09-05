18087b4aba60f487968fcdc989857e61fcda1b2d
Grachev Anton <antonio.grachev@gmail.com>
2021-09-05 20:43:39 +0300

    Fixed critical error

--- number_systems.py
+++ number_systems.py
@@ -2,19 +2,21 @@ def convert_base(num, from_base=10, to_base=10, again=False):
     try:
         from_base = int(from_base)
     except:
-        return 'Введите корректное значение исходной системы'
+        return 'Ошибка: введите корректное значение системы'
 
     try:
         to_base = int(to_base)
     except:
-        return 'Введите корректное значение получаемой системы'
+        return 'Ошибка: введите корректное значение системы'
+
+    num = str(num)
 
     for i in num:
         if i.islower():
             num = num.replace(i, i.upper())
 
     if num.count('.') > 1 or num.count(',') > 1:
-        return 'Введите корректную дробь'
+        return 'Ошибка: введите корректную дробь'
 
     frac = ''
     if '.' in num:
@@ -27,21 +29,21 @@ def convert_base(num, from_base=10, to_base=10, again=False):
             i = ord(i) - 55
         try:
             if int(i) > from_base:
-                return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+                return 'Ошибка: число содержит недопустимые для системы счисления символы'
         except:
-            return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+            return 'Ошибка: число содержит недопустимые для системы счисления символы'
 
     for i in frac:
         if i.isalpha():
             i = ord(i) - 55
         try:
             if int(i) > from_base:
-                return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+                return 'Ошибка: указанное число содержит недопустимые для системы счисления символы'
         except:
-            return 'Указанное число содержит недопустимые для исходной системы счисления символы'
+            return 'Ошибка: указанное число содержит недопустимые для системы счисления символы'
 
     if from_base < 2 or from_base > 36 or to_base < 2 or to_base > 36:
-        return 'Основание системы счисления не может быть меньше 2 или больше 36'
+        return 'Ошибка: основание системы счисления не может быть меньше 2 или больше 36'
 
     if from_base == to_base:
         return num
@@ -64,8 +66,9 @@ def convert_base(num, from_base=10, to_base=10, again=False):
                 res += chr(55 + tmp) if tmp % to_base > 9 else str(tmp)
                 n += 1
                 if frac_tmp == '0':
+                    print(res)
                     return str(res)
-            if again == False:
+            if not again:
                 res, s = res.split('.')
                 i = (s + s).find(s, 1, -1)
                 if i != -1:
@@ -88,11 +91,17 @@ def convert_base(num, from_base=10, to_base=10, again=False):
                     res += (ord(frac[i]) - 55) * (from_base ** -(i + 1))
                 else:
                     res += int(frac[i]) * (from_base ** -(i + 1))
+            if not again:
+                res, s = str(res).split('.')
+                i = (s + s).find(s, 1, -1)
+                if i != -1:
+                    res += '.(' + s[:i] + ')'
+                else:
+                    res += '.' + s
         return str(res)
 
     elif from_base != 10 and to_base != 10:
         return convert_base(convert_base(num + '.' + frac, from_base, 10, True), 10, to_base)
 
-
-#num, from_base, to_base = input().split()
-#print(convert_base(num, from_base, to_base))
+# num, from_base, to_base = input().split()
+# print(convert_base(num, from_base, to_base))
